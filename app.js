0fdb8e37b36efca4a55adb92308503cd9c775b43
David Berka <davidberka2005@gmail.com>
2022-02-18 15:09:36 +0100

    Second Commit

--- app.js
+++ app.js
@@ -1,50 +1,65 @@
+function scrollHandler(overlay) {
+    if (window.pageYOffset > 50 && window.innerWidth < 1330 && window.innerWidth > 768) {
+        overlay.classList.add('active') 
+    } else {
+        overlay.classList.remove('active') 
+    }
+}
+
+function showEventsHandler(item, eventsBoxDate, eventsBoxCategories) {
+    const eventsContainers = document.querySelectorAll('.calendar_category_events');
+
+    for (eventContainer of eventsContainers) {
+        eventContainer?.replaceChildren();
+    }
+
+    const dataDate = item?.dataset['date'];
+    const dataMen = {name: 'Muži', events: item?.dataset['men']};
+    const dataWomen = {name: 'Ženy', events: item?.dataset['women']};
+    const dataYouth =  {name: 'Mládež', events: item?.dataset['youth']};
+    const dataJuniors =  {name: 'Junioři', events: item?.dataset['juniors']};
+
+    const dataCategory = [dataMen, dataWomen, dataYouth, dataJuniors];
+
+    eventsBoxDate.textContent = dataDate;
+
+    eventsBoxCategories?.forEach((category, index) => {
+        if (dataCategory[index].events) {
+            const categoryTitle = category?.querySelector('h4');
+            categoryTitle.textContent = dataCategory[index].name;
+            category.style.display = 'block';
+
+            const events = dataCategory[index].events.split(', ');
+            for (eventItem of events) {
+                const liElement = document.createElement('li');
+                liElement.textContent = eventItem;
+
+                category?.querySelector('.calendar_category_events').appendChild(liElement);
+            }
+        } else {
+            category.style.display = 'none';
+        }
+    })
+}
+
 function hoverCalendarItemHandler() {
     const calendarItems = document.querySelectorAll('.calendar_table_item');    
+    const overlay = document.querySelector('.onScroll_overlay');
+
+    window.addEventListener('scroll', () => scrollHandler(overlay))
 
     const eventsBoxDate = document.querySelector('.calendar_col2 h3');
     const eventsBoxCategories = document.querySelectorAll('.calendar_category');
 
-    calendarItems.forEach(item => {
-        item.addEventListener('mouseenter', () => {
-            const dataDate = item?.dataset['date'];
-            const dataMen = {name: 'Muži', events: item?.dataset['men']};
-            const dataWomen = {name: 'Ženy', events: item?.dataset['women']};
-            const dataYouth =  {name: 'Mládež', events: item?.dataset['youth']};
-            const dataJuniors =  {name: 'Junioři', events: item?.dataset['juniors']};
+    eventsBoxCategories?.forEach(category => category.style.display = 'none');
 
-            const dataCategory = [dataMen, dataWomen, dataYouth, dataJuniors];
-
-            eventsBoxDate.textContent = dataDate;
-
-            eventsBoxCategories.forEach((category, index) => {
-                if (dataCategory[index].events) {
-                    const categoryTitle = category.querySelector('h4');
-                    categoryTitle.textContent = dataCategory[index].name;
-                    category.style.display = 'block';
-
-                    const events = dataCategory[index].events.split(', ');
-                    for (eventItem of events) {
-                        const liElement = document.createElement('li');
-                        liElement.textContent = eventItem;
-
-                        category.querySelector('.calendar_category_events').appendChild(liElement);
-                    }
-                } else {
-                    category.style.display = 'none';
-                }
-            })
-
-        }) 
-
-        item.addEventListener('mouseleave', () => {
-            const eventsContainers = document.querySelectorAll('.calendar_category_events');
-
-            for (eventContainer of eventsContainers) {
-                eventContainer.replaceChildren();
-            }
-        })
+    calendarItems?.forEach(item => {
+        if (window.innerWidth > 768) {
+            item?.addEventListener('mouseover', () => showEventsHandler(item, eventsBoxDate, eventsBoxCategories)) 
+        } else {
+            item?.addEventListener('click', () => showEventsHandler(item, eventsBoxDate, eventsBoxCategories)) 
+        }
     })
-
 }
 
 hoverCalendarItemHandler();
